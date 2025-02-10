d87f734a6e0270cf1982e99cc1e2960040967443
Kristiyan Kirilov <kirilov1905@gmail.com>
2025-02-10 18:10:58 +0200

    navigation bar created, css set

--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily:{
+        burtons: "burtons"
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
