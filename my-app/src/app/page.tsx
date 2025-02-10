910583976a78669bafaed1150cdd58b27141256f
Kristiyan Kirilov <kirilov1905@gmail.com>
2025-02-10 19:16:23 +0200

    project section

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,14 +1,21 @@
 import {BsFillMoonStarsFill} from 'react-icons/bs';
 import {AiFillGithub, AiFillLinkedin} from 'react-icons/ai';
 import Image from 'next/image';
-import deved from '../../public/dev-ed-wave.png';
+import profilePhoto from '../../public/profile_photo.jpg';
+import web1 from '../../public/web1.png';
+import web2 from '../../public/web2.png';
+import web3 from '../../public/web3.png';
+import web4 from '../../public/web4.png';
+import { useState } from 'react';
 
 export default function Home() {
+    const [darkMode, setDarkMode] = useState(false);
+    
   return (
     <div>
-      <main className="bg-white px-10">
+      <main className="bg-white px-10 md:px-20 lg:px-40">
         <section className="min-h-screen">
-          <nav className="py-10 mb-12 flex justify-between">
+          <nav className="py-10 mb-6 flex justify-between">
             <h1 className="text-xl font-burtons">Kristiyan's Portfolio</h1>
             <ul className="flex items-center">
                 <li>
@@ -20,9 +27,12 @@ export default function Home() {
             </ul>
           </nav>
           <div className="text-center p-10">
-                <h2 className="text-5xl py-2 text-teal-600 font-medium">Kristiyan Kirilov</h2>
-                <h3 className="text-2xl py-2">Junior Full Stack Developer</h3>
-                <p className="text-md py-5 leading-8 text-gray-800 max-w-96 mx-auto">
+                <h2 className="text-5xl py-2 text-teal-600 font-medium  md:text-6xl">Kristiyan Kirilov</h2>
+                <div className="relative bg-gradient-to-b from-teal-500 rounded-full w-60 h-60 my-10 mx-auto overflow-hidden"> 
+                     <Image src={profilePhoto} alt="Myself" layout="fill" objectFit="cover" />
+                </div>
+                <h3 className="text-2xl py-2 md:text-3xl">Junior Full Stack Developer</h3>
+                <p className="text-md py-5 leading-8 text-gray-800 max-w-96 mx-auto md:text-xl">
                     Currently graduated SoftUni student, who is looking forward to improve the learned knowledge in web development. 
                     On the other excellent student at TU-Varna in second course and member of the university programming team.  
                 </p>
@@ -31,9 +41,16 @@ export default function Home() {
             <a href="https://github.com/KristiyanKKirilov"><AiFillGithub/></a>
             <a href="https://www.linkedin.com/in/kristiyan-kirilov-797931302/"><AiFillLinkedin/></a>
           </div>
-          <div className="relative bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 mx-auto overflow-hidden"> 
-            <Image src={deved} alt="Myself" layout="fill" objectFit="cover" />
-          </div>
+         
+        </section>
+        <section>
+            <h3 className="text-3xl py-1">Portfolio</h3>
+            <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+                <div className="basis-1/3 flex-1"><Image className="rounded-lg object-cover w-full h-full" src={web1} alt="FirtProject"/></div>
+                <div className="basis-1/3 flex-1"><Image className="rounded-lg object-cover w-full h-full" src={web2} alt="SecondProject"/></div>
+                <div className="basis-1/3 flex-1"><Image className="rounded-lg object-cover w-full h-full" src={web3} alt="ThirdProject"/></div>
+                <div className="basis-1/3 flex-1"><Image className="rounded-lg object-cover w-full h-full" src={web4} alt="FourthProject"/></div>
+            </div>
         </section>
       </main>
     </div>
